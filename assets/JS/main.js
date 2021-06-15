9bc734f69ebc0aad6d38f111055647c9b113937a
virinarl <vrodriguezl@fi.uba.ar>
2021-06-14 23:19:03 -0300

    arrays

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,7 +1,13 @@
-//Función para hacer la primer letra mayus de un String
-function capitalized(string) {
-  return string.charAt(0).toUpperCase() + string.slice(1);
-}
+const diasSemana = [
+  "Domingo",
+  "Lunes",
+  "Martes",
+  "Miércoles",
+  "Jueves",
+  "Viernes",
+  "Sábado",
+];
+
 class Oferta {
   constructor(nombre, dias, porcentaje) {
     this.nombre = nombre;
@@ -9,30 +15,46 @@ class Oferta {
     this.porcentaje = parseFloat(porcentaje / 100);
   }
 }
-
+//Ofertas de distintos lugares
 const comunidad = new Oferta("comunidad", ["Martes", "Miércoles"], "15");
+const superFinDeSemana70 = new Oferta(
+  "Super Fin de Semana",
+  ["Sábado", "Domingo"],
+  "70"
+);
 
+//Cuando agregue más descuentos, esta función va a tener un Switch para elegir entre los distintos caminos a elegir de acuerdo al descuento
 function descuentoComunidad(usaComunidad, diaSemana, ticket) {
   if (usaComunidad === "S" && comunidad.dias.includes(diaSemana)) {
     total = ticket * (1 - comunidad.porcentaje);
     descuento = ticket * comunidad.porcentaje;
+  } else {
+    total = ticket;
+    descuento = 0;
   }
+}
+
+function getTodayName() {
+  let fecha = new Date();
+  let getWeekDay = fecha.getDay();
+
+  return getWeekDay;
 }
 
 let ejecutar = "S";
 
 while (ejecutar === "S") {
-  //inicializando variables locales
   const comunidad = prompt("Usa comunidad? S/N").toUpperCase();
-  const dia = capitalized(prompt("Qué día es hoy?"));
   const monto = parseInt(prompt("De cuánto es tu ticket?"));
 
-  //ejecutando función
+  //Obtengo el día de la semana
+  const dia = diasSemana[getTodayName()];
+
   descuentoComunidad(comunidad, dia, monto);
 
   //comunicando resultado
-  alert("El total a pagar es: " + total);
-  alert("El descuento obtenido es: " + descuento);
+  alert("El total a pagar es: $" + total);
+  alert("El descuento obtenido es: $" + descuento);
 
   // actualizando ejecutar
   ejecutar = prompt("Querés estimar otro ticket? S/N").toUpperCase();
