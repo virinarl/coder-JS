5231f8404912bcb9739b17f7cd6af2294ff19b04
virinarl <vrodriguezl@fi.uba.ar>
2021-07-29 22:41:45 -0300

    adding funcionality to preview red coins

--- games/FourInLine/main.js
+++ games/FourInLine/main.js
@@ -4,6 +4,7 @@ class Connect4 {
     this.COLS = 7;
     this.selector = selector;
     this.createBoard();
+    this.setupEvents();
   }
 
   createBoard() {
@@ -12,10 +13,38 @@ class Connect4 {
       const $row = $("<div>").addClass("row");
       $board.append($row);
       for (let col = 0; col < this.COLS; col++) {
-        const $col = $("<div>").addClass("col empty");
+        const $col = $("<div>")
+          .addClass("col empty")
+          .attr("data-col", col)
+          .attr("data-row", row);
         $row.append($col);
       }
     }
+  }
+
+  setupEvents() {
+    const $board = $(this.selector);
+
+    function findLastEmpty(col) {
+      const cells = $(`.col[data-col='${col}']`);
+      for (let i = cells.length - 1; i >= 0; i--) {
+        const $cell = $(cells[i]);
+        if ($cell.hasClass("empty")) {
+          return $cell;
+        }
+      }
+      return null;
+    }
+
+    $board.on("mouseenter", ".col.empty", function () {
+      const col = $(this).data("col");
+      const $lastEmpty = findLastEmpty(col);
+      $lastEmpty.addClass("preview-red");
+    });
+
+    $board.on("mouseleave", ".col", function () {
+      $(".col").removeClass("preview-red");
+    });
   }
 }
 
